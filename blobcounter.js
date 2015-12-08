7292ef108fa824e9094d423e0c5b4ae0fcbf12c6
Amir Keshavarz <amirkekh@gmail.com>
2015-12-08 10:42:38 +0330

    Code Quality

--- blobcounter.js
+++ blobcounter.js
@@ -1,4 +1,3 @@
-
 /*
  * blobcounter.js v1.0
  * https://github.com/amirhkkh/blobcounter.js
@@ -24,39 +23,42 @@ blobcounter.prototype.detect = function(radius) {
 	var height = c.height;
 	var width = c.width;
 	var particles = [];
-	
+	var idx;
+	var idp;
+	var particles_neighbor;
+	var y_start, y_end, x_start, x_end;
 	// Running through all of the pixels
 	for (var y = 0; y < height; y++) {
 		for (var x = 0; x < width; x++) {
 			
 			// Because every pixels has four value ( r,g,b,a )
-			var idx = (width * y + x) << 2;
+			idx = (width * y + x) << 2;
 			
 			// Using number of pixel instead of x and y to make the job easier
-			var idp = (width * y + x);
+			idp = (width * y + x);
 			
 			// Check if red value is 0 . We want black dots but we don't need to check all four of them because we trust the input
-			if (imgData.data[idx] == 0) {
-				var particles_neighbor = [];
+			if (imgData.data[idx] === 0) {
+				particles_neighbor = [];
 				
 				// Now we run through all of known blobs to search if any of them has a neighbor member of our current pixel
 				for (var i = 0; i < particles.length; i++) {
 					
 					// TODO : make the radius area into a circle instead of a square
-					var y_start = y - radius;
-					var y_end = y + 1;
+					y_start = y - radius;
+					y_end = y + 1;
 
-					var x_start = x - radius;
-					var x_end = x + radius + 1;
+					x_start = x - radius;
+					x_end = x + radius + 1;
 					
 					// Now we check every neighbor pixel to check if any of them is member of any blob
 					neighborhoodwatch: for (var y2 = y_start; y2 < y_end; y2++) {
 
 						for (var x2 = x_start; x2 < x_end; x2++) {
-							var idx = (width * y2 + x2);
+							idx = (width * y2 + x2);
 
 							var res1 = particles[i].indexOf(idx);
-							if (res1 != -1) {
+							if (res1 !== -1) {
 								particles_neighbor.push(i);
 								
 								// If we find a neighbor we break the neighborhoodwatch label in order to save resources
@@ -69,7 +71,7 @@ blobcounter.prototype.detect = function(radius) {
 				}
 				
 				// If we didn't find any neighbor particle we define a new blob
-				if (particles_neighbor.length == 0) {
+				if (particles_neighbor.length === 0) {
 					particles.push([ idp ]);
 					
 					// If there was a neighbor blob we add the pixel to it
@@ -111,11 +113,11 @@ blobcounter.prototype.detect = function(radius) {
 		// Calculating center of gravity based on average Xs and Ys
 		for (var p = 0; p < particles[b].length; p++) {
 
-			var y = Math.floor(particles[b][p] / width);
-			var x = particles[b][p] - (y * width);
+			var yp = Math.floor(particles[b][p] / width);
+			var xp = particles[b][p] - (yp * width);
 
-			particlex += x;
-			particley += y;
+			particlex += xp;
+			particley += yp;
 		}
 		
 		var particleavgx = Math.round(particlex / particles[b].length);
